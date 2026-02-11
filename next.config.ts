9732e78020c43369e31449471325209a80462b73
Argostroloji <argostroloji@gmail.com>
2026-02-11 22:35:31 +0300

    fix: move outputFileTracingIncludes from experimental to root config

--- next.config.ts
+++ next.config.ts
@@ -1,12 +1,9 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  experimental: {
-    // @ts-expect-error - feature is supported but types are missing in this version
-    outputFileTracingIncludes: {
-      '/': ['./content/**/*'],
-      '/api/**/*': ['./content/**/*'],
-    },
+  outputFileTracingIncludes: {
+    '/': ['./content/**/*'],
+    '/api/**/*': ['./content/**/*'],
   },
 };
 
